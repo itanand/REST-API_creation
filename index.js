ce53a461363bdfdec348ab6651c4584d2aa601f4
Anand Mohan <anand1505mohan@gmail.com>
2022-10-11 03:33:43 -0700

    api_creatiion

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.json());
+
+app.listen(PORT, () =>
+console.log(`Server Running on port: http://localhost:${PORT}`));
+
